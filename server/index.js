97f9fcfb28945ad9b7e807e5079c7aea7d2bdcdf
Peter Dolan <chinup.peter@gmail.com>
2019-07-16 18:32:59 -0500

    added dotenv

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+var GoogleStrategy = require('passport-google-oauth2').Strategy;
+require('dotenv').config();
 
 const app = express();
 
@@ -8,8 +10,8 @@ app.use(express.static(path.join(__dirname, '../dist')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
+process.env.CLIENT_ID;
+debugger;
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Your app is sparkling on port ${PORT}!`));
