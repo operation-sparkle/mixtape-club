cf9eed67eac4bfedadf7459b422ed445ded377d3
Peter Dolan <chinup.peter@gmail.com>
2019-07-17 11:22:31 -0500

    created sample search data

---
+++ sample-search-data.js
@@ -0,0 +1,342 @@
+{
+    "kind": "youtube#searchListResponse",
+        "etag": "\"Bdx4f4ps3xCOOo1WZ91nTLkRZ_c/raqQKj0f1CXxZfILDxp75xKxZk0\"",
+            "nextPageToken": "CAoQAA",
+                "regionCode": "US",
+                    "pageInfo": {
+        "totalResults": 365977,
+            "resultsPerPage": 10
+    },
+    "items": [
+        {
+            "kind": "youtube#searchResult",
+            "etag": "\"Bdx4f4ps3xCOOo1WZ91nTLkRZ_c/BX-e6aKCFe0jie86txslcn0a0UY\"",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "4D2qcbu26gs"
+            },
+            "snippet": {
+                "publishedAt": "2009-10-25T19:00:49.000Z",
+                "channelId": "UC5HlVQ64L25lubnxJaSTriA",
+                "title": "The Stone Roses - I Wanna Be Adored (Official Video)",
+                "description": "The Stone Roses' official music video for 'I Wanna Be Adored'. Click to listen to The Stone Roses on Spotify: http://smarturl.it/StoneRSpotify?IQid=TSRIWBA As ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/4D2qcbu26gs/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/4D2qcbu26gs/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/4D2qcbu26gs/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "StoneRosesVEVO",
+                "liveBroadcastContent": "none"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "\"Bdx4f4ps3xCOOo1WZ91nTLkRZ_c/yY5ons_XHhg9-FvbeDyAzRjS688\"",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "kmAZWKdCvmI"
+            },
+            "snippet": {
+                "publishedAt": "2008-05-09T21:48:44.000Z",
+                "channelId": "UCP9NBX8ETt65JvvF9j7adVQ",
+                "title": "The Stone Roses - I Wanna Be Adored",
+                "description": "The Stone Roses - I Wanna Be Adored.",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/kmAZWKdCvmI/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/kmAZWKdCvmI/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/kmAZWKdCvmI/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "burnleybadguy",
+                "liveBroadcastContent": "none"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "\"Bdx4f4ps3xCOOo1WZ91nTLkRZ_c/jgbS_QeIz1HMo2VjOeEP25pspcA\"",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "qR2c_W6zue8"
+            },
+            "snippet": {
+                "publishedAt": "2014-11-09T02:41:45.000Z",
+                "channelId": "UCccFm9vNZz5u1deqhWhN2Yw",
+                "title": "I Wanna Be Adored (Remastered)",
+                "description": "Provided to YouTube by Sony Music Entertainment I Wanna Be Adored (Remastered) · The Stone Roses The Stone Roses (20th Anniversary Collector's Edition) ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/qR2c_W6zue8/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/qR2c_W6zue8/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/qR2c_W6zue8/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "The Stone Roses - Topic",
+                "liveBroadcastContent": "none"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "\"Bdx4f4ps3xCOOo1WZ91nTLkRZ_c/Kc74y3piMTFn7BsViRl_6OxZBJk\"",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "fh1kaHVt-lA"
+            },
+            "snippet": {
+                "publishedAt": "2009-10-25T19:08:46.000Z",
+                "channelId": "UC5HlVQ64L25lubnxJaSTriA",
+                "title": "The Stone Roses - I Wanna Be Adored (Live In Blackpool)",
+                "description": "Music video by The Stone Roses performing I Wanna Be Adored. (C) 1995 Silvertone Records Ltd.",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/fh1kaHVt-lA/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/fh1kaHVt-lA/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/fh1kaHVt-lA/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "StoneRosesVEVO",
+                "liveBroadcastContent": "none"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "\"Bdx4f4ps3xCOOo1WZ91nTLkRZ_c/cpI9n_y5AHuoXftT-LuVMeVPiCU\"",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "ZVX8j9AsuV4"
+            },
+            "snippet": {
+                "publishedAt": "2018-06-08T19:40:44.000Z",
+                "channelId": "UCccFm9vNZz5u1deqhWhN2Yw",
+                "title": "I Wanna Be Adored",
+                "description": "Provided to YouTube by Sony Music Entertainment I Wanna Be Adored · The Stone Roses / 石玫瑰合唱團 The Very Best Of The Stone Roses ℗ 1989 Silvertone ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/ZVX8j9AsuV4/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/ZVX8j9AsuV4/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/ZVX8j9AsuV4/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "The Stone Roses - Topic",
+                "liveBroadcastContent": "none"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "\"Bdx4f4ps3xCOOo1WZ91nTLkRZ_c/_9O5s0PPiyywJluj4iKOSfeZ9-U\"",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "8Kc2EiZxMT8"
+            },
+            "snippet": {
+                "publishedAt": "2019-06-18T23:15:41.000Z",
+                "channelId": "UCgDjsVW9t2XNN2m65bh-tCw",
+                "title": "Metallica - I Wanna Be Adored [Live] - 6.18.2019 - Etihad Stadium - Manchester, England",
+                "description": "Front row video I took of Metallica's Rob Trujillo and Kirk Hammett performing a cover of “I Wanna Be Adored'” (originally by The Stone Roses) at the Etihad ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/8Kc2EiZxMT8/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/8Kc2EiZxMT8/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/8Kc2EiZxMT8/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "Erynn Halvorson",
+                "liveBroadcastContent": "none"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "\"Bdx4f4ps3xCOOo1WZ91nTLkRZ_c/FK5l6n8YMyD8DZ-mXvFnvzzP3dE\"",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "E7dq6w9klDg"
+            },
+            "snippet": {
+                "publishedAt": "2006-08-13T12:20:10.000Z",
+                "channelId": "UCyDdGTD2f26SzZIJYxESHSg",
+                "title": "Ian Brown - I Wanna Be Adored (T in the Park)",
+                "description": "Ian Brown - I Wanna Be Adored (T in the Park)",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/E7dq6w9klDg/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/E7dq6w9klDg/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/E7dq6w9klDg/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "E K",
+                "liveBroadcastContent": "none"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "\"Bdx4f4ps3xCOOo1WZ91nTLkRZ_c/KQrjgQuetT4nHMlNOC-PMtzMUxc\"",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "v9p_pRXV2Mg"
+            },
+            "snippet": {
+                "publishedAt": "2017-06-25T00:23:22.000Z",
+                "channelId": "UClBFjuit5YG3XjiXet2tsGQ",
+                "title": "The Stone Roses - I Wanna Be Adored Live @ Hampden 2017",
+                "description": "The Stone Roses - I Wanna Be Adored Live @ Hampden 2017.",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/v9p_pRXV2Mg/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/v9p_pRXV2Mg/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/v9p_pRXV2Mg/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "Live Gigs UK",
+                "liveBroadcastContent": "none"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "\"Bdx4f4ps3xCOOo1WZ91nTLkRZ_c/hB3gJacbx16p3lKNB6pS1mVe3ZM\"",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "FcpuA4AuIFA"
+            },
+            "snippet": {
+                "publishedAt": "2016-06-19T09:03:28.000Z",
+                "channelId": "UCiNJf9HW-fcGiudRYk5VEug",
+                "title": "The Stone Roses - I wanna be adored, Etihad Manchester 18/06/16",
+                "description": "Spine tingling opening to the Roses show at Manchester City's Etihad Stadium on Saturday 18th June 2016. Proper goose bumps... PLEASE NOTE - FOR ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/FcpuA4AuIFA/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/FcpuA4AuIFA/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/FcpuA4AuIFA/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "Ian Corcoran",
+                "liveBroadcastContent": "none"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "\"Bdx4f4ps3xCOOo1WZ91nTLkRZ_c/uac6VO9cF0tA1UA-F_VJ2r-fnoE\"",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "vD2xi-AbPPc"
+            },
+            "snippet": {
+                "publishedAt": "2012-08-28T17:43:37.000Z",
+                "channelId": "UCbbiGNZVkmGjeaedArYJB3A",
+                "title": "The Stone Roses   I Wanna Be Adored Live   SNUB TV",
+                "description": "",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/vD2xi-AbPPc/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/vD2xi-AbPPc/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/vD2xi-AbPPc/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "floydpink26",
+                "liveBroadcastContent": "none"
+            }
+        }
+    ]
+}
---
+++ sample-video-data.js
@@ -0,0 +1,18 @@
+// CREATE TABLE`playlist`(
+//     `id` INTEGER NOT NULL AUTO_INCREMENT,
+//     `userId` INTEGER NOT NULL DEFAULT,
+//     `aSideLinks` CHAR(1000) NOT NULL DEFAULT ' ',
+//     `bSideLinks` CHAR(10000) NULL DEFAULT NULL,
+//     `aTitles` CHAR(1000) NOT NULL,
+//     `bTitles` CHAR(255) NOT NULL DEFAULT ' ',
+//     PRIMARY KEY(`id`)
+// );
+
+playlist = {
+    id: 1,
+    userId: 1,
+    aSideLinks: ['4D2qcbu26gs', "r52KqG4G678", "Rht7rBHuXW8"],
+    bSideLinks: ["8ahU-x-4Gxw", "H1Zm6E6Sy4Y", "fpsOOrwF558"],
+    aTitles: ["The Stone Roses - I Wanna Be Adored (Official Video)", "Doja Cat - Juicy (Audio)", "ROSALÍA - \"Malamente\" (Official Video) Download & stream “Malamente”"],
+    bTitles: ["New Order - Age Of Consent", "Boy Harsher — Pain", "Total Control - Flesh War"],
+};
