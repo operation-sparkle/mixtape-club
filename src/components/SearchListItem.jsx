a126fdac8b73f16302164faf679590337eaee3fe
Rachel Brandsness <brandsnessr@gmail.com>
2019-07-17 11:22:43 -0500

    Create hover effect for SearchListItem

--- src/components/SearchListItem.jsx
+++ src/components/SearchListItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchListItem = (props) => {
     const { searchResult } = props;
     return (
-        <li className="list-group-item">{searchResult}</li>
+        <li className="list-group-item search-item">{searchResult}</li>
     )
 };
 
