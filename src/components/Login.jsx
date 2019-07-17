5c7f41b97e0d52a9f3ef14e2d4df103f18c5ca02
Rachel Brandsness <brandsnessr@gmail.com>
2019-07-16 19:29:29 -0500

    Add Bootstrap and re-create login component with Bootstrap styles

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Button';
 
 const LoginBox = () => {
     return (
-        <div className="login-box">
-        <h3>Login to create and share mixtapes.</h3>
-            <a href="/auth/google"><button>Sign In with Google</button></a>
+        
+        <div className="card">
+            <div className="card-header">Login</div>
+            <div className="card-body">
+                <h5 className="card-title">Welcome to Mixtape Club!</h5>
+                <p className="card-text">You must be logged in to create and share mixtapes.</p>
+                <a href="/auth/google" className="btn btn-primary">Sign In with Google</a>
+            </div>
         </div>
     )
 };
