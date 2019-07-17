ce8a8c48dac10657615ea6eec65c4c20a002e062
Peter Dolan <chinup.peter@gmail.com>
2019-07-16 22:35:54 -0500

    successfully getting response from google

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth2').Strategy;
 require('dotenv').config();
 
@@ -9,9 +10,59 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../dist')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-process.env.CLIENT_ID;
-debugger;
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+//this function will use the user info to access database info
+    done(null, obj);
+});
+
+
+//beginning of google authentication
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/auth/google/callback',
+    passReqToCallback: true,
+},
+    function (req, token, tokenSecret, profile, done) {
+        // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        //     return done(err, user);
+        // });
+        console.log(profile);
+       process.nextTick(function(){
+           return done(null, profile);
+       })
+    }
+))
+
+
+// This will call google's authentication
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope:
+            ['email', 'profile']
+    }
+    ));
+
+// Redirect on success or failure
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    }));
+
+app.get('/', (req, res) => {
+    console.log(req);
+    res.render('/');
+})
+
+
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Your app is sparkling on port ${PORT}!`));
