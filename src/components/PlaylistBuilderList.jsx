5d09943fa33a95d3147ca8abbef3aa9beab5fd75
Rachel Brandsness <brandsnessr@gmail.com>
2019-07-18 14:27:53 -0500

    Style PlaylistBuilder Component

--- src/components/PlaylistBuilderList.jsx
+++ src/components/PlaylistBuilderList.jsx
@@ -1,24 +1,45 @@
 import React from 'react';
 
+import LisaFrankenstein from '../assets/img/tapes/lisa-frankenstein-tape.gif';
+
 const PlaylistBuilderList = () => {
     return (
-        <div className="border border-info col-md-8 playlist-builder">
-        <h3>Playlist Builder:</h3>
-        <p>
-            
-            <button className="btn btn-info" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                Show All Songs</button>
-        </p>
-        <div className="collapse" id="collapseExample">
-            <div className="card card-body">
-                    <ul className="list-group col-sm-10 col-md-8 search-list">
-                        <li className="list-group-item">Song1</li>
-                        <li className="list-group-item">Song2</li>
-                        <li className="list-group-item">Song3</li>
-                        <li className="list-group-item">Song4</li>
-                    </ul>
+        <div className="border border-info playlist-builder shadow-sm p-3 mb-5 bg-white rounded">
+        <div className="row">
+                <div className="col-md-4" style={{ marginTop: ".5rem" }}>
+                    <h5 style={{ textAlign: "center" }}>Playlist Builder:</h5>
+                    <img className="col-md-12" src={LisaFrankenstein}/>
+                    <p style={{ margin: ".5rem 1rem" }}><b>Label:</b>User Label Goes Here</p>
+                 </div>
+                <div className="col-2" style={{marginTop: "1rem"}}>
+                    <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                        <a className="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Side A</a>
+                        <a className="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Side B</a>
+                    </div>
+                </div>
+                <div className="col-5">
+                    <div className="tab-content" id="v-pills-tabContent">
+                        <div className="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                            <ul className="list-group list-group-flush builder-tracks">
+                                <li className="list-group-item track-li">Track 1 A</li>
+                                <li className="list-group-item track-li">Track 2 A</li>
+                                <li className="list-group-item track-li">Track 3 A</li>
+                                <li className="list-group-item track-li">Track 4 A</li>
+                                <li className="list-group-item track-li">Track 5 A</li>
+                            </ul>
+                        </div>
+                        <div className="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+                            <ul className="list-group list-group-flush builder-tracks">
+                                <li className="list-group-item track-li">Track 1 B</li>
+                                <li className="list-group-item track-li">Track 2 B</li>
+                                <li className="list-group-item track-li">Track 3 B</li>
+                                <li className="list-group-item track-li">Track 4 B</li>
+                                <li className="list-group-item track-li">Track 5 B</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
     </div>
     )
 }
