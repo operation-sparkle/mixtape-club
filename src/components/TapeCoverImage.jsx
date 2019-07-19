bf6bc9d0fb0c9e339776f9a2ffabf85c69a7a3be
Rachel Brandsness <brandsnessr@gmail.com>
2019-07-19 18:21:01 -0500

    Add and render cassette deck image for player

--- src/components/TapeCoverImage.jsx
+++ src/components/TapeCoverImage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import LisaFrankenstein from '../assets/img/tapes/lisa-frankenstein-tape.gif';
+import CassetteDeck from '../assets/img/cassette-deck.png';
 
 const TapeCoverImage = () => {
     return (
         <div>
-            <img className="card-img-top col-md-10 tape-cover-image" src={LisaFrankenstein} alt="Card image cap" />
+            <img className="card-img-top col-md-12 tape-deck-image" src={CassetteDeck} alt="Card image cap" />
+            <img className="card-img-top col-md-6 tape-cover-image" src={LisaFrankenstein} alt="Card image cap" />
         </div>
     )
 }
