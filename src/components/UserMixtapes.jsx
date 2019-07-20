856e4bd6b255611c76ffd63b7f564e6dc4fac1d9
Rachel Brandsness <brandsnessr@gmail.com>
2019-07-20 15:20:40 -0500

    fix user mixtapes border color

--- src/components/UserMixtapes.jsx
+++ src/components/UserMixtapes.jsx
@@ -7,7 +7,7 @@ const UserMixtapesList = (props) => {
     console.log(props);
     return (
         <ul className="list-group col-12 mx-auto my-mixtape-list">
-            <li className="list-group-item active bg-info">My Mixtapes:</li>
+            <li className="list-group-item active  border border-info bg-info">My Mixtapes:</li>
             <li className="list-group-item">Mixtape 1</li>
             <li className="list-group-item">Mixtape 2</li>
             <li className="list-group-item">Mixtape 3</li>
