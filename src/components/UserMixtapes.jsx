f47c6585e34274dee704f872a9fef4e8dab18cbd
Rachel Brandsness <brandsnessr@gmail.com>
2019-07-20 13:28:37 -0500

    Create player styles for tablets

--- src/components/UserMixtapes.jsx
+++ src/components/UserMixtapes.jsx
@@ -6,7 +6,7 @@ const UserMixtapesList = (props) => {
     const { searchResults } = props;
     console.log(props);
     return (
-        <ul className="list-group col-12 col-md-8 my-mixtape-list">
+        <ul className="list-group col-12 my-mixtape-list">
             <li className="list-group-item active bg-info">My Mixtapes:</li>
             <li className="list-group-item">Mixtape 1</li>
             <li className="list-group-item">Mixtape 2</li>
