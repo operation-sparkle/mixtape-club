3aec0e23e963351cc29d778bece4677ac7bec508
Rachel Brandsness <brandsnessr@gmail.com>
2019-07-19 21:06:08 -0500

    experiment with icon sizing

--- src/components/PlayerSongList.jsx
+++ src/components/PlayerSongList.jsx
@@ -19,20 +19,20 @@ const PlayerSongList = (props) => {
                     <div className="tab-content" id="v-pills-tabContent">
                         <div className="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
                             <ul className="list-group list-group-flush builder-tracks">
-                                <li className="list-group-item track-li">{aSideTitles[0]} <FontAwesomeIcon icon={faMinusCircle} style={{ float: 'right', color: '#17a2b8' }} /></li>
-                                <li className="list-group-item track-li">{aSideTitles[1]} <FontAwesomeIcon icon={faMinusCircle} style={{ float: 'right', color: '#17a2b8' }} /></li>
-                                <li className="list-group-item track-li">{aSideTitles[2]} <FontAwesomeIcon icon={faMinusCircle} style={{ float: 'right', color: '#17a2b8' }} /></li>
-                                <li className="list-group-item track-li">{aSideTitles[3]} <FontAwesomeIcon icon={faMinusCircle} style={{ float: 'right', color: '#17a2b8' }} /></li>
-                                <li className="list-group-item track-li">{aSideTitles[4]} <FontAwesomeIcon icon={faMinusCircle} style={{ float: 'right', color: '#17a2b8' }} /></li>
+                                <li className="list-group-item track-li">{aSideTitles[0]} </li>
+                                <li className="list-group-item track-li">{aSideTitles[1]} </li>
+                                <li className="list-group-item track-li">{aSideTitles[2]} </li>
+                                <li className="list-group-item track-li">{aSideTitles[3]} </li>
+                                <li className="list-group-item track-li">{aSideTitles[4]} </li>
                             </ul>
                         </div>
                         <div className="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
                             <ul className="list-group list-group-flush builder-tracks">
-                                <li className="list-group-item track-li">{bSideTitles[0]} <FontAwesomeIcon icon={faMinusCircle} style={{ float: 'right', color: '#17a2b8' }} /></li>
-                                <li className="list-group-item track-li">{bSideTitles[1]} <FontAwesomeIcon icon={faMinusCircle} style={{ float: 'right', color: '#17a2b8' }} /></li>
-                                <li className="list-group-item track-li">{bSideTitles[2]} <FontAwesomeIcon icon={faMinusCircle} style={{ float: 'right', color: '#17a2b8' }} /></li>
-                                <li className="list-group-item track-li">{bSideTitles[3]} <FontAwesomeIcon icon={faMinusCircle} style={{ float: 'right', color: '#17a2b8' }} /></li>
-                                <li className="list-group-item track-li">{bSideTitles[4]} <FontAwesomeIcon icon={faMinusCircle} style={{ float: 'right', color: '#17a2b8' }} /></li>
+                                <li className="list-group-item track-li">{bSideTitles[0]}</li>
+                                <li className="list-group-item track-li">{bSideTitles[1]}</li>
+                                <li className="list-group-item track-li">{bSideTitles[2]}</li>
+                                <li className="list-group-item track-li">{bSideTitles[3]}</li>
+                                <li className="list-group-item track-li">{bSideTitles[4]}</li>
                             </ul>
                         </div>
                     </div>
@@ -42,13 +42,13 @@ const PlayerSongList = (props) => {
     )
 }
 
-<ul className="list-group col-sm-10 col-md-8 search-list">
-    <li className="list-group-item active bg-info">Tracks:</li>
-    <li className="list-group-item">Track 1</li>
-    <li className="list-group-item">Track 2</li>
-    <li className="list-group-item">Track 3</li>
-    <li className="list-group-item">Track 4</li>
-</ul>
+// <ul className="list-group col-sm-10 col-md-8 search-list">
+//     <li className="list-group-item active bg-info">Tracks:</li>
+//     <li className="list-group-item">Track 1</li>
+//     <li className="list-group-item">Track 2</li>
+//     <li className="list-group-item">Track 3</li>
+//     <li className="list-group-item">Track 4</li>
+// </ul>
 
 
 export default PlayerSongList;
