3215b60ce30edb2d9b934ec3d118c754e218b34d
Rachel Brandsness <brandsnessr@gmail.com>
2019-07-20 15:17:45 -0500

    Center flip button, redirect title click in navbar to mixtape player route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,8 @@ const Navigation = (props) => {
 
     return (
         <nav className="navbar navbar-expand-md fixed-top navbar-dark bg-info">
-            <a className="navbar-brand" href="#">Mixtape Club</a>
+            
+            <Link to="/mixtape-player" className="navbar-brand">Mixtape Club</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
