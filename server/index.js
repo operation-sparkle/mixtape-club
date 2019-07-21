0f13a3ca3caa4188daff507b7823b1fb93d29605
franco3445 <francoappss@gmail.com>
2019-07-21 10:44:20 -0500

    Comments added to index.js
    Both server/index.js and database/index.js

--- server/index.js
+++ server/index.js
@@ -6,11 +6,29 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const db = require('../database/index.js');
-const ObjectId = require('mongodb').ObjectID;
+
+/**
+ * express required to aid in in handeling request made to server
+ * session required to aid with passport request for google authentication
+ * path required to aid in redirects to avoid landing on incorrect endpoint
+ * axios required to send requests
+ * bodyParse required to retrieve information from body while avoiding chunks
+ * passport required in retrieving info from google authentication
+ * GoogleStrategy required to retireve user's google infromation to store users
+ * db required as a path to our database commands
+ */
 
 require('dotenv').config();
 
+/**
+ * app rename to aid in functioncalls
+ */
+
 const app = express();
+
+/**
+ * middleware assigned to app to aid in any incoming requests
+ */
 
 app.use(express.static(path.join(__dirname, '../dist')));
 app.use(bodyParser.json());
@@ -23,13 +41,21 @@ passport.serializeUser((user, done) => {
   done(null, user);
 });
 
+/**
+ * Function created to use user information to access database information
+ */
+
 passport.deserializeUser((obj, done) => {
 // this function will use the user info to access database info
   done(null, obj);
 });
 
+/**
+ * Beginning of Google authenticaion
+ * passport using newly created instance of GoogleStrategy
+ * db.findCreate called after to store information to DB.
+ */
 
-// beginning of google authentication
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
@@ -42,33 +68,47 @@ passport.use(new GoogleStrategy({
   process.nextTick(() => done(null, profile));
 })));
 
-// This will call google's authentication
+/**
+ * Get request calling Google's authenticaion
+ * defining the scope of infromation to retrieve
+ */
 app.get('/auth/google',
   passport.authenticate('google', {
     scope:
             ['email', 'profile'],
   }));
 
-// Redirect on success or failure
+/**
+* Get request used to redirect users based on success or failure of login
+*/
 app.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: 'http://localhost:3000/login' }),
   (req, res) => {
     res.redirect('http://localhost:3000/mixtape-player');
   });
 
+/**
+ * Get request handler used to retrieve users information from request sent to server
+ */
 
 app.get('/user/', (req, res) => {
   res.send(req.user);
 });
+
+/**
+ * Get request handler used to sign user out of application using the request sent to server
+ */
 
 app.get('/logout', (req, res) => {
   req.logOut();
   console.log('logged out');
   res.end('logged out');
 });
-// when refreshing on different endpoints, our page would crash.
-// https://tylermcginnis.com/react-router-cannot-get-url-refresh/
-// Read article above for explanation
+
+/**
+ * Get request handler to db to findCreate user based on response from database
+ */
+
 app.get('/getUser', (req, res) => {
   db.findCreate(req.query, (info, response) => {
     console.log(response);
@@ -76,9 +116,15 @@ app.get('/getUser', (req, res) => {
   });
 });
 
+/**
+ * Get Request handler used to find all playlists from database
+ * user information taken from request coming to server
+ * return the model found, if found
+ */
+
 app.get('/userPlaylists', (req, res) => {
   const { id, displayName } = req.user;
-   console.log(displayName);
+  console.log(displayName);
   db.getAllPlaylists({ userId: id }, (info, response) => {
     console.log(response);
     const data = { response, displayName };
@@ -86,9 +132,21 @@ app.get('/userPlaylists', (req, res) => {
   });
 });
 
+/**
+ * Get request handler used to redirect users to mixtape-player endpoint after login
+ */
+
 app.get('/', (req, res) => {
   res.redirect('http://localhost:3000/mixtape-player');
 });
+
+
+/**
+ * Get request handler used as a catchall to help react router
+ * when refreshing on different endpoints, our page would crash.
+ * https://tylermcginnis.com/react-router-cannot-get-url-refresh/
+ * Read article above for explanation
+ */
 
 app.get('/*', (req, res) => {
   if (req.path !== '/auth/google/callback') {
@@ -104,6 +162,12 @@ app.get('/*', (req, res) => {
   }
 });
 
+/**
+ * Post Request handler to aid in updating music player
+ * orignally made when designing app
+ * stretch goal
+ */
+
 app.post('/update', (req, res) => {
   // need to figure out how we are sending info to endpoint
   const filter = { userId: 'CHANGE THE FILTER SOMEHOW FILL_ME_IN' };
@@ -113,6 +177,10 @@ app.post('/update', (req, res) => {
     res.end('Playlist Updated');
   });
 });
+
+/**
+ * Post Request handler used to collect and store information through db call
+ */
 
 app.post('/store', (req, res) => {
   // need to figure out how we are sending info to endpoint
@@ -133,6 +201,12 @@ app.post('/store', (req, res) => {
   });
 });
 
+/**
+ * Post request handler used to git playlist _id number
+ * _id is used to sort through playlist when displaying sharable mixtape-player
+ * please note the underscore before id
+ */
+
 app.post('/getLink', (req, res) => {
   const { key } = req.body;
   const filter = { aSideLinks: key };
@@ -146,6 +220,11 @@ app.post('/getLink', (req, res) => {
     }
   });
 });
+
+/**
+ * Post request used to load information onto mixtape-player 
+ * retrieves info from database to render on screen
+ */
 
 app.post('/mixtape-player/', (req, res) => {
   // need to do this dynamically
@@ -184,6 +263,10 @@ app.post('/mixtape-player/', (req, res) => {
   });
 });
 
+/**
+ * Post request used to search information based on user input
+ * axios.get request sent to google's api to retrieve snippet from youtube containing music
+ */
 
 app.post('/search', (req, res) => {
   const queryString = req.body.query;
